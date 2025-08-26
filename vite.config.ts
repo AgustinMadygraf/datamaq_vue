90a387865316bdf4a97d4f74da356877c8ab0744
AgustinMadygraf <agustin.mtto.madygraf>
2025-08-25 23:45:25 -0300

    Add server proxy configuration to vite.config.ts for API routing

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,12 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: './', 
+  server: {
+    proxy: {
+      '/datamaq_php/backend/api/v1/': {
+        target: 'http://127.0.0.1:5000', 
+        changeOrigin: true,
+      }
+    }
+  }
 })
